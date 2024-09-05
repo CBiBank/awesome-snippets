890d3b0421e370eea225be1ad1b187a9744c0fb2
yangxiaoping <2878847069@qq.com>
2024-09-05 15:31:00 +0800

    feat: add node tool chalk

---
+++ src/scripts/node-tool/4.node-tool-chalk.js
@@ -0,0 +1,8 @@
+// chalk@^4.1.0
+const chalk = require('chalk')
+
+chalk.red().green().blue().yellow()
+
+chalk.bgRed().bgGreen().bgBlue().bgYellow()
+
+chalk.bold().italic().underline().strikethrough()
